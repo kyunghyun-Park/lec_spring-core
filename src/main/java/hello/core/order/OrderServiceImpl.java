b9ebcd030acd9d057b566b01aef151854331b8ed
hyunyyxp <hyunyyxp@fusionsoft.co.kr>
2022-09-20 14:46:08 +0900

    김영한 - 스프링 핵심 원리 commit

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        //discountPolicy가 없었다면 할인에대한 변경을 이 곳에서 바꿔야할 수도 있었음.(단일체계원칙 잘 지켜진 것)
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+
+    }
+}
